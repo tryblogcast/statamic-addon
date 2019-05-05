bb4b5476a7d63908bdc4e85547bf560a4034a473
Miguel Piedrafita <soy@miguelpiedrafita.com>
2019-05-06 01:15:17 +0200

    wip

--- resources/lang/en/settings.php
+++ resources/lang/en/settings.php
@@ -1,5 +1,5 @@
 <?php
 return [
     'blogcast_token' => 'Blogcast Token',
-    'blogcast_token_instruct' => "Your Blogcast API Token. Used to add new posts to Blogcast if this option is enabled."
+    'blogcast_token_instruct' => "Your Blogcast API Token. If you don't have an API token yet, you can create one on the Developer tab."
 ];
